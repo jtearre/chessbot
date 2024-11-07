4d999bc08503993e9c5c386666f6562dca48d0e8
MongoRaise <70969022+MongoRaise@users.noreply.github.com>
2024-11-07 10:54:26 -0800

    again

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
     // Initialize Stockfish using the CDN link you provided
     console.log("Creating Stockfish worker...");
-const stockfish = new Worker("https://cdnjs.cloudflare.com/ajax/libs/stockfish.js/10.0.2/stockfish.js");
+const stockfish = new Worker("https://cdn.jsdelivr.net/npm/@niklasf/stockfish.wasm/stockfish.wasm.js");
 
     // Test if Stockfish responds to an initial command
     stockfish.onmessage = function(event) {
